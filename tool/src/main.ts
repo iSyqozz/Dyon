628e2a6cf0cef24b7a66684b09072a2c3d330c26
Fares Hassen <fares.hassen512@gmail.com>
2023-05-14 19:27:06 +0100

    test

--- tool/src/main.ts
+++ tool/src/main.ts
@@ -1,5 +1,4 @@
-
-
+import html2canvas from "html2canvas"
 
 //variables and constants.
 const black_coordinates = new Set([
@@ -58,6 +57,7 @@ const bucket_option = document.querySelector('.bucket') as HTMLElement;
 const eraser_option = document.querySelector('.eraser') as HTMLElement;
 const palette_container = document.querySelector('.colors') as HTMLElement;
 const root = document.documentElement;
+const downloadButton = document.querySelector('.download') as HTMLElement;
 
 
 
@@ -65,15 +65,24 @@ var next = '#d7dadb';
 var mat:Array<Array<HTMLElement>> = [] 
 
 var curr_color:HTMLElement = document.createElement('div');
+curr_color.style.backgroundColor = 'rgb(140, 255, 222)'
+
 var prev_color:HTMLElement = document.createElement('div');
+prev_color.style.backgroundColor = 'rgb(140, 255, 222)'
+
 var curr_mode:HTMLElement = pen_option;
 var prev_mode:HTMLElement = pen_option;
+let isDrawing:boolean = false;
 
+var initial_colors = new Map<HTMLElement, string>();
+var divToCoordsMap = new Map<HTMLElement,Array<number>>();
+var snapshots:Array<string> = []; 
 
 
 //utility functions
+
+//resetting the grid
 function reset_grid(){
-    console.log('hi');
     var next = '#d7dadb';
     for (let i = 0; i < 30; i++) {
         
@@ -81,8 +90,8 @@ function reset_grid(){
         mat[i][0].style.backgroundColor = next;
 
         for (let j = 1; j < 30; j++) {
-            if(next === 'white'){next = '#d7dadb'}
-            else{ next = 'white'}
+            if(next === '#f5f5f5'){next = '#d7dadb'}
+            else{ next = '#f5f5f5'}
 
             mat[i][j].style.backgroundColor = next;
 
@@ -93,16 +102,88 @@ function reset_grid(){
     }
 }
 
+//using a snapshot
+function from_snapshot(){
 
-//script start
+    try{
+        for (let i = 0; i < 30; i++) {
+            for (let j = 0; j < 30; j++) {
+                const div = mat[i][j];
+                div.style.backgroundColor = snapshots[snapshots.length-1]
+            }
+        }
+    }catch(e){
+        console.log('no undo available.')
+    }
+}
+
+//drawing the pixel
+function draw_pixel(pixel:HTMLElement){
+    pixel.style.backgroundColor = curr_color.style.backgroundColor
+}
+
+//erasing a pixel
+function erase_pixel(pixel:HTMLElement){
+    pixel.style.backgroundColor = initial_colors.get(pixel)!;
+}
+function bucketFill(clickedDiv: HTMLElement) {
+    const clickedColor = clickedDiv.style.backgroundColor;
+    const newColor = curr_color.style.backgroundColor
+    
+    if (clickedColor === newColor) {
+      return;
+    }
+    
+    const visitedDivs = new Set<HTMLElement>();
+    const fillAdjacent = (div: HTMLElement) => {
+      if (!visitedDivs.has(div)) {
+        visitedDivs.add(div);
+        
+        console.log(clickedColor,div.style.backgroundColor)
+
+
+        const coords = divToCoordsMap.get(div)!;
+        const topDiv = coords[0] > 0 ? mat[coords[0] - 1][coords[1]] : null;
+        const bottomDiv = coords[0] < mat.length - 1 ? mat[coords[0] + 1][coords[1]] : null;
+        const leftDiv = coords[1] > 0 ? mat[coords[0]][coords[1] - 1] : null;
+        const rightDiv = coords[1] < mat[0].length - 1 ? mat[coords[0]][coords[1] + 1] : null;
+        
+        if (div.style.backgroundColor === clickedColor) {
+
+            div.style.backgroundColor = newColor;
+            if (topDiv) fillAdjacent(topDiv);
+            if (bottomDiv) fillAdjacent(bottomDiv);
+            if (leftDiv) fillAdjacent(leftDiv);
+            if (rightDiv) fillAdjacent(rightDiv);
+
+        }else if(['rgb(215, 218, 219)','rgb(245, 245, 245)'].includes(clickedColor) && ['rgb(215, 218, 219)','rgb(245, 245, 245)'].includes(div.style.backgroundColor)){
+
+            div.style.backgroundColor = newColor;
+            if (topDiv) fillAdjacent(topDiv);
+            if (bottomDiv) fillAdjacent(bottomDiv);
+            if (leftDiv) fillAdjacent(leftDiv);
+            if (rightDiv) fillAdjacent(rightDiv);
+
+        }
+      }
+    }
+    
+    fillAdjacent(clickedDiv);
+  }
+
+//initialization
 function main(){
     
-    //setting up grid for first time
+    //setting up grid for first time and adding event listeners
     
     for (let i = 0; i < 30; i++) {
         const div = document.createElement('div');
+
 	    container.appendChild(div);
         div.style.backgroundColor = next;
+        
+        initial_colors.set(div, next);
+        divToCoordsMap.set(div,[i,0])
 
         if (black_coordinates.has(`[${i},0]`)){
             div.style.backgroundColor = 'black';
@@ -114,12 +195,16 @@ function main(){
         
         for (let j = 1; j < 30; j++) {
             
-            if(next === 'white'){next = '#d7dadb'}
-            else{ next = 'white'}
+            if(next === '#f5f5f5'){next = '#d7dadb'}
+            else{ next = '#f5f5f5'}
 
             const div = document.createElement('div');
             container.appendChild(div);
             div.style.backgroundColor = next;
+            
+            initial_colors.set(div, next);
+            divToCoordsMap.set(div,[i,j])
+
 
             if (black_coordinates.has(`[${i},${j}]`)){
                 div.style.backgroundColor = 'black';
@@ -129,11 +214,16 @@ function main(){
         }
     }
 
+
     //setting up color picker
     for (let i = 0; i<32 ; i++){
         const div = document.createElement('div');
 	    palette_container.appendChild(div);
         div.style.backgroundColor = colors[i];
+
+        if (i === 0){
+            div.classList.add('first-color')
+        }
 
 
         div.addEventListener('click',()=>{
@@ -149,8 +239,6 @@ function main(){
 
         })
     }
-
-    
 
 
     //changing modes
@@ -199,9 +287,80 @@ function main(){
     })
 
 
+  
+    // Add mousedown event listener
+    container.addEventListener("mousedown", (e) => {
+      isDrawing = true;
+      const current_div = e.target as HTMLElement
+
+      if (curr_mode === pen_option){
+        draw_pixel(current_div);
+      }else if (curr_mode === eraser_option){
+        erase_pixel(current_div);
+      }else{
+        bucketFill(current_div);
+      }
+    });
+  
+    // Add mousemove event listener
+    container.addEventListener("mousemove", (e) => {
+      if (isDrawing) {
+        console.log('dragging');
+        const current_div = e.target as HTMLElement
+        
+        if (curr_mode === pen_option){
+            draw_pixel(current_div);
+          }else if (curr_mode === eraser_option){
+            erase_pixel(current_div);
+          }
+      }
+    });
+  
+    // Add mouseup event listener
+    container.addEventListener("mouseup", () => {
+        console.log('out from mouseup');
+        isDrawing = false;
+    });
+  
+
 
     //resetting canvas
     reset.addEventListener('click',reset_grid);
+
+    //adding download capability
+
+    downloadButton.addEventListener('click', async () => {
+        
+        for (var i=0 ; i<30; i++){
+            for (var j=0; j<30; j++){
+                console.log('fading')
+                const div = mat[i][j];
+                if (['rgb(215, 218, 219)','rgb(245, 245, 245)'].includes(div.style.backgroundColor)){
+                    div.style.backgroundColor = 'rgba(0,0,0,0)';
+                }
+            }
+        }
+        
+        const canvas = await html2canvas(container!);
+        const dataURL = canvas.toDataURL('image/png');
+        const link = document.createElement('a');
+        link.download = 'drawing.png';
+        link.href = dataURL;
+        link.click();
+        link.target = '_blank';
+
+        for (var i=0 ; i<30; i++){
+            for (var j=0; j<30; j++){
+                console.log('resetting')
+                const div = mat[i][j];
+                console.log(div.style.backgroundColor);
+                if (['rgba(0, 0, 0, 0)'].includes(div.style.backgroundColor)){
+                    div.style.backgroundColor = initial_colors.get(div)!
+                }
+            }
+        }
+
+      });
 
 
 }
